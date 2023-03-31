5a33693b4124620e07068cd40bf35efa3ddd8ae0
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2023-03-31 23:51:35 +0300

    Fix r8 register getting restored.

--- Source/DivaModLoader/StrArrayImp.asm
+++ Source/DivaModLoader/StrArrayImp.asm
@@ -48,7 +48,6 @@ public ?implOfGetStr@@YAPEBDH@Z
     push rdx
     push rsi
     push rdi
-    push r8
     push r9
     push r10
     push r11
@@ -68,7 +67,6 @@ public ?implOfGetStr@@YAPEBDH@Z
     pop r11
     pop r10
     pop r9
-    pop r8
     pop rdi
     pop rsi
     pop rdx
